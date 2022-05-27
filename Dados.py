34af88529f0e999857ab3de75e76f05dcd13ccfa
DanDaDante <101757805+DanDaDante@users.noreply.github.com>
2022-05-27 18:48:45 -0500

    Add files via upload

--- Dados.py
+++ Dados.py
@@ -1,32 +1,47 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+import random
+
 class Dados(App):
-	#Este es el constructor
+    #Este es el constructor
     def __init__(self,**kwargs):
-		# llamar al constructor de la clase base (App)
+        #Llamar al constructor de la clase base (App)
         super().__init__(**kwargs)
-        # Defino un atributo
-        
+        #Defino un atributo
+
+    #Eventos
+    def btnAccion_dado(self,obj):
+        #Generar el numero en el dado        
+        num = random.randint(1,6)
+        #Cambiar la etiqueta (atributo)
+        self.lblNumDado.text = str(num)
+
+    #Todos los elementos visuales se definene o encadenan aqui
     def build(self):
-         #Vamos a definir un layout
+        #Vamos a definir un layout
         gdl_principal = GridLayout(rows=3,cols=1)
-        lbltitulo = Label(text='Aplicacion Dado')
-        gdl_principal.add_widget(lbltitulo)
+        #Etiqueta
+        lblTitulo = Label(text="Aplicacion Dado")
+        gdl_principal.add_widget(lblTitulo)
         #Grid medio
         gdl_medio = GridLayout(cols=2)
-        lblresultado = Label(text='Resultado')
-        gdl_medio.add_widget(lblresultado)
-        lblnumdado = Label(text="")
-        gdl_medio.add_widget(lblnumdado)
-        gdl_principal.add_widget(gdl_medio)        
+        lblResultado = Label(text="Resultado")
+        gdl_medio.add_widget(lblResultado)
+        lblNumDado = Label(text="6")
+        gdl_medio.add_widget(lblNumDado)
+        gdl_principal.add_widget(gdl_medio)
         #Boton
-        btnaccion = Button(text="Presioname!!")
-        gdl_principal.add_widget(btnaccion)
-        self. gdl_principal =  gdl_principal
-        return  gdl_principal
-        
-if __name__== '__main__':
+        btnAccion = Button(text="Presioname!!!")
+            #Enlazar
+        btnAccion.bind(on_press=self.btnAccion_dado) 
+        gdl_principal.add_widget(btnAccion)
+        #Atributos
+        self.lblNumDado = lblNumDado
+        self.gdl_principal = gdl_principal
+        return gdl_principal
+
+if __name__ == '__main__':
     D = Dados()
     D.run()
